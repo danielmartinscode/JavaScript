9cf8c142adff88e1de4eba7843bd49bd82ac054f
Daniel Martins de Oliveira <155649847+danielmartinscode@users.noreply.github.com>
2024-04-16 21:27:24 -0300

    updates

--- aula12ex/video game/script.js
+++ aula12ex/video game/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function jogar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var idade = 30
+
+    msg.innerHTML = `Você tem ${idade} anos deve jogar...`
+    if (idade < 18) {
+        img.src = 'snes.png'
+        document.body.style.background = '#cdcdce'
+    } else if (idade < 25) {
+        img.src = 'neo.png'
+        document.body.style.background = '#d2c2b2'
+    } else {
+        img.src = 'mega.png'
+        document.body.style.background = '#141414'
+    }
+
+}
