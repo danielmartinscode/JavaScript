ac14f02e8114dead6540647c8b1fb274c72bd8fe
Daniel Martins de Oliveira <155649847+danielmartinscode@users.noreply.github.com>
2024-04-19 21:00:34 -0300

    aprendendo sobre array

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function tabuada(){
+    var num = document.getElementById('txtnum')
+    var tabuada = document.getElementById('resultado')
+
+    if (num.value.length == 0 ) {
+        alert('Por favor, digite um número.')
+        tabuada.innerHTML = 'Digite um número para começar!'
+    } else {
+         num = Number(num.value)
+         tabuada.innerHTML = ''
+
+    for (var i = 0; i <= 10; i++) {
+        var resultado = num * i
+        tabuada.innerHTML += `${num} x ${i} = ${resultado}<br>`
+        }
+    }
+}
